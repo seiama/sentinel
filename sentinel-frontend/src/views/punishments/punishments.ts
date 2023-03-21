8b1d584575ad10120773a2669a7cd5531f685e42
Riley Park <riley.park@seiama.com>
2023-03-20 23:41:16 -0700

    chore: move api url to env variable

--- sentinel-frontend/src/views/punishments/punishments.ts
+++ sentinel-frontend/src/views/punishments/punishments.ts
@@ -1,11 +1,11 @@
 export const getPunishments = async () => {
-  const response = await fetch("http://localhost:8080/v1/punishments")
+  const response = await fetch(`${import.meta.env.VITE_SENTINEL_API_URL}/v1/punishments`)
   if (!response.ok) throw new Error(`Error status code: ${response.status}`)
   return await response.json()
 }
 
 export const getPunishment = async (id: string) => {
-  const response = await fetch(`http://localhost:8080/v1/punishment/${id}`)
+  const response = await fetch(`${import.meta.env.VITE_SENTINEL_API_URL}/v1/punishment/${id}`)
   if (!response.ok) throw new Error(`Error status code: ${response.status}`)
   return await response.json()
 }
