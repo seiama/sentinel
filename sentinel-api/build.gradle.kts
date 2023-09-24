33558cfefee5e77e2a750adf17fe95a04bd94be9
Riley Park <riley.park@seiama.com>
2023-09-24 12:52:03 -0700

    chore: update dependencies

--- sentinel-api/build.gradle.kts
+++ sentinel-api/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("sentinel.conventions")
-  id("org.springframework.boot") version "3.0.5"
-  id("io.spring.dependency-management") version "1.1.0"
+  id("org.springframework.boot") version "3.2.1"
+  id("io.spring.dependency-management") version "1.1.4"
   id("com.gorylenko.gradle-git-properties") version "2.4.1"
 }
 
@@ -11,7 +11,7 @@ tasks.named<Jar>(JavaPlugin.JAR_TASK_NAME) {
 
 dependencies {
   implementation(project(":sentinel-common"))
-  implementation("com.discord4j:discord4j-core:3.3.0-20230626.170702-107")
+  implementation("com.discord4j:discord4j-core:3.3.0-20230903.204616-114")
   implementation("com.seiama:commons:1.0.0-SNAPSHOT")
   implementation("com.seiama:functional:1.0.0-SNAPSHOT")
   implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
--- sentinel-common/build.gradle.kts
+++ sentinel-common/build.gradle.kts
@@ -3,17 +3,17 @@ plugins {
 }
 
 dependencies {
-  api("com.discord4j:discord4j-core:3.3.0-20230626.170702-107")
-  api("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-  api("com.google.guava:guava:31.1-jre")
+  api("com.discord4j:discord4j-core:3.3.0-20230903.204616-114")
+  api("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+  implementation("com.google.guava:guava:33.0.0-jre")
   api("com.seiama:commons:1.0.0-SNAPSHOT")
   api("com.seiama:functional:1.0.0-SNAPSHOT")
   api("io.projectreactor.netty:reactor-netty-core:1.1.8")
   api("jakarta.persistence:jakarta.persistence-api:3.1.0")
   implementation("net.time4j:time4j-base:5.9.3")
-  api("org.mongodb:bson:4.10.1")
-  api("org.springframework.data:spring-data-mongodb:4.1.1")
-  testImplementation(platform("org.junit:junit-bom:5.9.3"))
+  api("org.mongodb:bson:4.10.2")
+  api("org.springframework.data:spring-data-mongodb:4.2.2")
+  testImplementation(platform("org.junit:junit-bom:5.10.1"))
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testImplementation("org.junit.jupiter:junit-jupiter-params")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
