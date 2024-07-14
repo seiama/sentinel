069f783df79977077b543874f4110348c2188893
Riley Park <riley.park@seiama.com>
2024-07-14 16:29:28 -0700

    chore: update dependencies

--- sentinel-importer/build.gradle.kts
+++ sentinel-importer/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
   implementation(project(":sentinel-common"))
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
-  implementation("com.google.guava:guava:33.0.0-jre")
-  implementation("it.unimi.dsi:fastutil:8.5.12")
-  implementation("org.mongodb:mongodb-driver-reactivestreams:4.11.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
+  implementation("com.google.guava:guava:33.2.1-jre")
+  implementation("it.unimi.dsi:fastutil:8.5.13")
+  implementation("org.mongodb:mongodb-driver-reactivestreams:5.0.1")
 }
