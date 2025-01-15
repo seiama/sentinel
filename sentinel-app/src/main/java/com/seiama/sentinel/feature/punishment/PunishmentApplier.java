df73d96aaff6a4d28a169f0b59ad3e701d15ca68
Riley Park <rileysebastianpark@gmail.com>
2025-01-14 20:36:31 -0800

    chore: punishment application refactor

---
+++ sentinel-app/src/main/java/com/seiama/sentinel/feature/punishment/PunishmentApplier.java
@@ -0,0 +1,72 @@
+package com.seiama.sentinel.feature.punishment;
+
+import com.seiama.sentinel.command.Options;
+import com.seiama.sentinel.common.model.PunishmentModel;
+import com.seiama.sentinel.common.model.PunishmentRepository;
+import com.seiama.sentinel.feature.punishment.display.PunishmentMessages;
+import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.Interaction;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.User;
+import discord4j.discordjson.possible.Possible;
+import java.time.Instant;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+import reactor.function.TupleUtils;
+
+public final class PunishmentApplier {
+  private static final boolean ACTUALLY_APPLY_PUNISHMENT = true;
+
+  private PunishmentApplier() {
+  }
+
+  private static @NotNull Mono<?> notifyAndApply(
+    final Guild guild,
+    final User user,
+    final PunishmentRepository punishments,
+    final PunishmentModel.Complete punishment,
+    final PunishmentAction action
+  ) {
+    return Mono.whenDelayError(
+      user.getPrivateChannel()
+        .flatMap(channel -> channel.createMessage(PunishmentMessages.punishmentPunishedDirectMessageEmbed(punishment, guild)))
+        .flatMap(message -> punishments.update(punishment, new PunishmentModel.Partial.DirectMessageNotified() {
+          @Override
+          public Snowflake dmNotificationMessageId() {
+            return message.getId();
+          }
+        }))
+        // we don't actually care if we can't send a notification to the user
+        .onErrorResume(t -> Mono.empty()), // avoid possible 50007
+      ACTUALLY_APPLY_PUNISHMENT ? action.apply(guild, user, punishment) : Mono.empty()
+    );
+  }
+
+  public static @NotNull Mono<?> apply(
+    final ChatInputInteractionEvent event,
+    final Guild guild,
+    final PunishmentRepository punishments,
+    final PunishmentModel.Type type,
+    final PunishmentAction action
+  ) {
+    final Interaction interaction = event.getInteraction();
+    return event.deferReply().then(Options.user(event, Options.MEMBER)
+      .orElse(Mono.empty())
+      .zipWhen(user -> punishments.insert(PunishmentModel.Complete.create(
+        guild.getId(),
+        type,
+        Instant.now(),
+        interaction.getUser(),
+        user,
+        Options.string(event, Options.REASON)
+          .orElse(null),
+        false
+      )))
+      .flatMap(TupleUtils.function((user, punishment) -> Mono.whenDelayError(
+        notifyAndApply(guild, user, punishments, punishment, action),
+        event.editReply().withContent(Possible.of(Optional.of(PunishmentMessages.punishmentPunisherResponse(punishment))))
+      ))));
+  }
+}
