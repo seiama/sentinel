b9ed0425688e2b259112a4091906995085a4aa44
Doc <nachito94@msn.com>
2025-04-30 01:28:37 -0400

    feat: support cv2 in factoids

--- sentinel-app/src/main/java/com/seiama/sentinel/configuration/JacksonConfiguration.java
+++ sentinel-app/src/main/java/com/seiama/sentinel/configuration/JacksonConfiguration.java
@@ -1,7 +1,15 @@
 package com.seiama.sentinel.configuration;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.seiama.sentinel.common.jackson.CommonModule;
+import discord4j.common.JacksonResources;
+import discord4j.common.jackson.UnknownPropertyHandler;
+import discord4j.discordjson.possible.PossibleFilter;
+import discord4j.discordjson.possible.PossibleModule;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +21,13 @@ public class JacksonConfiguration {
   @Bean
   Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
     return builder -> builder.modules(
-      new JavaTimeModule(),
-      new CommonModule()
-    );
+        new PossibleModule(),
+        new JavaTimeModule(),
+        new CommonModule()
+      )
+      .failOnUnknownProperties(false)
+      .visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE)
+      .visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.PUBLIC_ONLY).featuresToEnable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION).serializationInclusion(JsonInclude.Value.construct(JsonInclude.Include.CUSTOM,
+        JsonInclude.Include.ALWAYS, PossibleFilter.class, null));
   }
 }
