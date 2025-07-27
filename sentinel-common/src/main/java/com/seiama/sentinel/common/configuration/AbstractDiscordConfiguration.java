3995d0529b79b27ee7875efcbe05c8e1a68eed76
Riley Park <rileysebastianpark@gmail.com>
2025-07-27 10:13:40 -0700

    wiretap

--- sentinel-common/src/main/java/com/seiama/sentinel/common/configuration/AbstractDiscordConfiguration.java
+++ sentinel-common/src/main/java/com/seiama/sentinel/common/configuration/AbstractDiscordConfiguration.java
@@ -1,5 +1,6 @@
 package com.seiama.sentinel.common.configuration;
 
+import discord4j.common.ReactorResources;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.ClientPresence;
@@ -11,10 +12,12 @@ import discord4j.gateway.intent.IntentSet;
 import discord4j.rest.RestClient;
 import discord4j.rest.service.ApplicationService;
 import discord4j.rest.util.AllowedMentions;
+import io.netty.handler.logging.LogLevel;
 import java.util.function.Consumer;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @NullMarked
 public abstract class AbstractDiscordConfiguration {
@@ -51,6 +54,12 @@ public abstract class AbstractDiscordConfiguration {
   private GatewayDiscordClient client(final String token, final Consumer<GatewayBootstrap<GatewayOptions>> gateway) {
     final GatewayBootstrap<GatewayOptions> bootstrap = DiscordClientBuilder.create(token)
       .setDefaultAllowedMentions(AllowedMentions.suppressEveryone())
+      .setReactorResources(new ReactorResources(
+        ReactorResources.DEFAULT_HTTP_CLIENT.get()
+          .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL),
+        ReactorResources.DEFAULT_TIMER_TASK_SCHEDULER.get(),
+        ReactorResources.DEFAULT_BLOCKING_TASK_SCHEDULER.get()
+      ))
       .build()
       .gateway()
       .setSharding(ShardingStrategy.recommended());
